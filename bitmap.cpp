0af2b5eab00cf11fa1d67e1fa8f055c419b53cbb
contrbas@gmail.com <K4rmU78B>
2018-08-13 13:06:48 +0300

    ALL

--- bitmap.cpp
+++ bitmap.cpp
@@ -45,5 +45,11 @@ bool Bitmap::write(string filename)
 
 void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue)
 {
+    uint8_t *pPixel = _pPixels.get(); // the whole screen;
 
+    pPixel += ( y * 3 ) * _width + (x * 3); // move to the correspondent location;
+
+    pPixel[0] = red;
+    pPixel[1] = green;
+    pPixel[2] = blue;
 }
--- main.cpp
+++ main.cpp
@@ -8,15 +8,28 @@ using namespace std;
 
 int main(void)
 {
-    Bitmap bitmap(800, 600);
+    int const WIDTH = 800;
+    int const HEIGHT = 600;
+    double min = 999999;
+    double max = -999999;
+
+    Bitmap bitmap(WIDTH, HEIGHT);
+
+    for(int y = 0; y < HEIGHT; y++)
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            double xFractol = 0;
+            double yFractol = 0;
+
+            if (xFractol < min) min = xFractol;
+            if (xFractol > max) max = xFractol;
+        }
+    }
+
+    cout << min << ',' << max << endl;
 
     bitmap.write("test.bmp");
-
-
-
-
-
-
 
     return (0);
 }
