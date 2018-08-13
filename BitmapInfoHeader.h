9c6840df75b6a43de861df6cc1ce32a5d852ceb4
contrbas@gmail.com <K4rmU78B>
2018-08-13 11:20:20 +0300

    ALL

--- BitmapInfoHeader.h
+++ BitmapInfoHeader.h
@@ -12,8 +12,8 @@ struct BitmapInfoHeader
     int32_t     headerSize{40};
     int32_t     width;
     int32_t     heigh;
-    int32_t     planes{1};
-    int32_t     bitsPerPixel{24};
+    int16_t     planes{1};
+    int16_t     bitsPerPixel{24};
     int32_t     compression{0};
     int32_t     dataSize{0};
     int32_t     horizotalResolution{2400};
@@ -21,8 +21,5 @@ struct BitmapInfoHeader
     int32_t     colors{0};
     int32_t     imprtantColors{0};
 };
-
-
-
 
 #endif
