eca61b3287df40c2b3838a8ce41ffba87e0791b7
contrbas@gmail.com <K4rmU78B>
2018-08-13 11:42:38 +0300

    ALL

--- bitmap.cpp
+++ bitmap.cpp
@@ -8,3 +8,8 @@ bool Bitmap::write(string filename)
 {
     return false;
 }
+
+void Bitmap::setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue)
+{
+    
+}
--- bitmap.h
+++ bitmap.h
@@ -2,6 +2,7 @@
 #define BITMAP_H
 
 #include <string>
+#include <cstdint>
 
 using namespace std;
 
@@ -14,6 +15,7 @@ private:
 public:
     Bitmap(int width, int height);
     bool write(string filename);
+    void setPixel(int x, int y, uint8_t red, uint8_t green, uint8_t blue);
     virtual ~Bitmap();
 };
 
